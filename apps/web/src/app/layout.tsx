646fe9a5fbcaaabefd4aa02e2efcea00ae83066b
Harsh Sahu <yesh8harsh+github@gmail.com>
2026-03-07 15:02:37 +0530

    stash before merge of "main" and "origin/main"

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
+import { TooltipProvider } from "@/web/components/ui/tooltip";
 import { cn } from "@/web/lib/utils";
 
 const nunitoSans = Nunito_Sans({ variable: "--font-sans" });
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html className={cn("font-sans", nunitoSans.variable)} lang="en">
-      <body className={`${nunitoSans.variable} antialiased`}>{children}</body>
+      <body className={`${nunitoSans.variable} antialiased`}>
+        <TooltipProvider>{children}</TooltipProvider>
+      </body>
     </html>
   );
 }
